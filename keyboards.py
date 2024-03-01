3fa6758f7e462620dbb83e12b51f77fc32e6936e
sunprizrak <sunprizrak@gmail.com>
2024-03-01 21:01:12 +0300

    update

--- keyboards.py
+++ keyboards.py
@@ -1,31 +1,5 @@
-from types import FunctionType
-from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
-
-
-def default_inline_kb(buttons: dict, adjust: int | tuple, factory, back_factory=None, back_text=None) -> InlineKeyboardMarkup:
-    kb = InlineKeyboardBuilder()
-
-    for elem in buttons:
-
-        if isinstance(elem, FunctionType):
-            old_elem = elem
-            new_elem = elem()
-            kb.button(text=new_elem, callback_data=factory(field=buttons[old_elem]))
-        else:
-            kb.button(text=elem, callback_data=factory(field=buttons[elem]))
-
-    if back_factory and back_text:
-        kb.button(text=back_text, callback_data=back_factory)
-
-    if isinstance(adjust, tuple):
-        kb.adjust(*adjust)
-    elif isinstance(adjust, int):
-        kb.adjust(adjust)
-
-    return kb.as_markup(
-        resize_keyboard=True,
-    )
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 def auth_kb():
